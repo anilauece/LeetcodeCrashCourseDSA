ca1af5e52ff62b4684cd2789b93935c9de39cd9e
anil kumar <anil.auece@gmail.com>
2023-08-04 07:27:00 +0530

    Create 2-2Palindrome.java

---
+++ 2Arrays and Strings/Code/Java/2-2Palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkIfPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+		    /* Odd length strings */
+      
+        System.out.println(solution.checkIfPalindrome("racecar")); // prints true
+		    System.out.println(solution.checkIfPalindrome("aslkd")); // prints false
+		
+		    /* Even length strings */
+		    System.out.println(solution.checkIfPalindrome("basdab")); // prints false
+		    System.out.println(solution.checkIfPalindrome("abassaba")); // prints true
+    }
+};
