7d10322aedfa96816b78aa4612a34bc85095dae0
anil kumar <anil.auece@gmail.com>
2023-08-15 16:16:27 +0530

    Update 2-2SortedArrayTargetSum.java

--- 2Arrays and Strings/Code/Java/2-2SortedArrayTargetSum.java
+++ 2Arrays and Strings/Code/Java/2-2SortedArrayTargetSum.java
@@ -26,10 +26,12 @@ class Main {
         Solution solution = new Solution();
 
         System.out.println(solution.checkForTarget(new int[]{1, 2, 4, 6, 8, 9, 14, 15}, 13)); // true
-		int[] nums2 = {1, 4, 7, 12, 16};
-		System.out.println(solution.checkForTarget(nums2, 15)); // false
-		int[] nums3 =  {3, 6, 21, 23, 25};
-		System.out.println(solution.checkForTarget(nums3, 27)); // true
+	
+	int[] nums2 = {1, 4, 7, 12, 16};
+	System.out.println(solution.checkForTarget(nums2, 15)); // false
+	
+	int[] nums3 =  {3, 6, 21, 23, 25};
+	System.out.println(solution.checkForTarget(nums3, 27)); // true
     }
 };
 
