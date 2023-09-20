d8c08ac1fd841721897cb7fa6d5d65528965b177
anil kumar <anil.auece@gmail.com>
2023-09-20 06:31:36 +0530

    Create 2-2IsSubsequence.java

---
+++ 2Arrays and Strings/Code/Java/2-2IsSubsequence.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+      Solution solution = new Solution();
+      
+      System.out.println(solution.isSubsequence("ace", "abcde")); // true
+
+	    System.out.println(solution.isSubsequence("aec", "abcde")); // false
+
+	    System.out.println(solution.isSubsequence("abc", "ahbgdc")); // true
+      
+      System.out.println(solution.isSubsequence("axc", "ahbgdc")); // false
+    }
+};
+
+// Compilation steps:
+// javac 2-2IsSubsequence.java
+// java Main
+
+// Time-complexity : O(n+m)
+// Space complexity: O(1)
