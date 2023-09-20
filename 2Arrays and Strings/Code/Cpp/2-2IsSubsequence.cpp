043dcacec932ece15b4bfb0c18f4964c678d3a85
anil kumar <anil.auece@gmail.com>
2023-09-20 08:15:59 +0530

    Update 2-2IsSubsequence.cpp

--- 2Arrays and Strings/Code/Cpp/2-2IsSubsequence.cpp
+++ 2Arrays and Strings/Code/Cpp/2-2IsSubsequence.cpp
@@ -12,3 +12,15 @@ public:
         return i == s.size();
     }
 };
+
+int main() {
+    Solution solution;
+    string s = "abc";
+    string t = "abcde";
+    
+    bool result = solution.isSubsequence(s, t);
+    
+    cout << (result ? "True" : "False") << endl;
+    
+    return 0;
+}
